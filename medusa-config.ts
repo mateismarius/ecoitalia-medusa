19f07654350ed39dd599dd05b240ba1298bdaed8
mateismarius <103111532+mateismarius@users.noreply.github.com>
2025-10-15 16:39:10 +0300

    fix env

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,14 +1,14 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils'
+import { defineConfig } from '@medusajs/framework/utils'
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL, // ← ADAUGĂ ASTA!
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors: process.env.STORE_CORS || "http://localhost:8000",
+      adminCors: process.env.ADMIN_CORS || "http://localhost:7001",
+      authCors: process.env.AUTH_CORS || "http://localhost:9000",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
